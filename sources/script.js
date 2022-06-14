860c50542eb370dea00c5dd7d90732a053febb0a
iness benaissa <ine.benss@gmail.com>
2022-06-14 19:55:56 +0200

    remove images > add Contact > start JS

--- sources/script.js
+++ sources/script.js
@@ -1,1 +1,10 @@
 length.css
+
+const $logo = document.querySelector('.logo')
+const $linkedin = document.querySelector('.linkedin')
+const $phone = document.querySelector('.phone')
+const $instagram = document.querySelector('.instagram')
+const $mail = document.querySelector('.mail')
+
+
+$logo.addEventListener
